9b2bad714fc82ebfd3cf85d3bc77f3dea78460c1
LinzhouLi <LinzhouLi@outlook.com>
2026-05-08 16:26:38 +0800

    update S2C-3D

--- S2C-3D/static/js/index.js
+++ S2C-3D/static/js/index.js
@@ -206,27 +206,10 @@ function setupQualitativeResults() {
     renderQualitativeScene(scenes[0], scenes, 0);
 }
 
-$(document).ready(function() {
-    // Check for click events on the navbar burger icon
-
-    var options = {
-		slidesToScroll: 1,
-		slidesToShow: 1,
-		loop: true,
-		infinite: true,
-		autoplay: true,
-		autoplaySpeed: 5000,
-    }
-
-	// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
-	
-    bulmaSlider.attach();
-    
+document.addEventListener('DOMContentLoaded', function() {
     // Setup video autoplay for carousel
     setupVideoCarouselAutoplay();
 
     // Setup qualitative results block from PPT-exported assets
     setupQualitativeResults();
-
-})
+});
